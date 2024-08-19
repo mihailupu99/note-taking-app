13280ad6e7333c0fe0dc743b373d13e309155f65
Mihai Lupu <mihailupu.dev@gmail.com>
2024-08-19 12:20:05 +0300

    Create add new note functionality.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 "use strict";
 
-let modalWindow = document.querySelector(".modal-window");
-let newNoteButton = document.querySelector(".new-note");
-let spanClose = document.querySelector(".btn-close");
+const modalWindow = document.querySelector(".modal-window");
+const newNoteButton = document.querySelector(".new-note");
+const spanClose = document.querySelector(".btn-close");
+const submitNote = document.querySelector(".submit-note");
+const noteForm = document.querySelector(".new-note-form");
+const noteContainer = document.querySelector(".notes-area");
 
 newNoteButton.onclick = function () {
   modalWindow.style.display = "block";
@@ -17,3 +20,30 @@ window.onclick = function (e) {
     modalWindow.style.display = "none";
   }
 };
+
+noteForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Form submission prevented");
+  const noteTitle = document.querySelector("#note-title").value;
+  const noteText = document.querySelector("#note-text").value;
+
+  console.log(noteTitle);
+  console.log(noteText);
+
+  let newNote = document.createElement("div");
+  let newNoteTitle = document.createElement("h2");
+  let newNoteContent = document.createElement("p");
+
+  newNote.classList.add("single-note");
+  newNoteTitle.classList.add("heading-secondary");
+  newNoteContent.classList.add("note-text");
+
+  newNoteTitle.textContent = noteTitle;
+  newNoteContent.textContent = noteText;
+
+  newNote.appendChild(newNoteTitle);
+  newNoteTitle.insertAdjacentElement("afterend", newNoteContent);
+  noteContainer.appendChild(newNote);
+
+  modalWindow.style.display = "none";
+});
